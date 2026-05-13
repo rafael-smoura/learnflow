88e2805046eb168a7f91267f2e2b051f8d822d10
rafael-smoura <rafaelsilvamoura166@gmail.com>
2026-05-13 13:16:54 -0300

    feat: add MVP page structure and favicon integration

--- app.py
+++ app.py
@@ -1,7 +1,29 @@
+"""
+App.py
+    Código principal da aplicação
+"""
 from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
+def welcome():
+    return render_template('welcome.html')
+
+@app.route('/main')
 def main():
-    return render_template('index.html')
+    return render_template('main.html')
+
+@app.route("/courses")
+def courses():
+    return render_template('courses.html')
+
+@app.route("/login")
+def login():
+    return render_template('login.html')
+
+@app.route("/about")
+def about():
+    return render_template('about.html')
+
+
